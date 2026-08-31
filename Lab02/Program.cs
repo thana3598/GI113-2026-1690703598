8bccec6a035609bd77791133a98ce0cf5ab8c068
thana3598 <tana.iq2008@gmail.com>
2026-08-31 17:09:08 +0700

    Lab03

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -10,7 +10,7 @@ namespace Lab02
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {   
             string bossName = "Kirin";
             char rank = 'S';
             int level = 7;
@@ -18,10 +18,12 @@ namespace Lab02
             int currentHp = 175;
             float attackPower = 42.5f;
             double critMultiplier = 1.75;
-            bool isBoss = true;
-            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            bool isBoss = true
+            //Part A Lab02
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");  
             Console.WriteLine($"Name: {bossName}");
             Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Level: {level}");
             Console.WriteLine($"HP: {currentHp} / {maxHp}");
             Console.WriteLine($"Attack Power: {attackPower}");
             Console.WriteLine($"Crit Multiplier: {critMultiplier}");
@@ -29,15 +31,23 @@ namespace Lab02
             Console.WriteLine();
 
             int hpPercent = currentHp * 100 / maxHp;
-            Console.WriteLine($"HP Percent: {hpPercent}");
+            Console.WriteLine($"HP Percent: {hpPercent}%    ");
             Console.WriteLine();
 
-            Console.WriteLine($"Kirin takes 60 damge!");
-            currentHp = currentHp = 60;
+            currentHp = currentHp - 60;
             Console.WriteLine();
 
             Console.WriteLine($"===== BOSS STATUS: AFTER DAMAGE =====");
             Console.WriteLine($"HP: { currentHp} / { maxHp}");
+            hpPercent = currentHp * 100 / maxHp;
+            Console.WriteLine($"HP Percent: {hpPercent}%");
+           
+            //Part B Lab02
+            string hero1Name = "GINMARU";
+            int hero1Hp = 100;
+            float hero1Atk = 19.7f;
+            double hero1CritRate = 0.15;
+            char hero1Rank = 'A';
         }
     }
 }
